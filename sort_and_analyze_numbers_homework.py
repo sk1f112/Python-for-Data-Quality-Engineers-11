5d2b10d24dcc7fd7c19fe52721c83e85fba61903
Vladyslav Ostapenko <vlad.ostapen15@gmail.com>
2025-02-24 21:12:34 +0200

    Some corrections

--- sort_and_analyze_numbers_homework.py
+++ sort_and_analyze_numbers_homework.py
@@ -1,20 +1,24 @@
 import random
 # Generate a list of 100 random numbers ranging from 0 to 1000
-list1 = [random.randint(0, 1000) for _ in range(100)]
+random_numbers = [random.randint(0, 1000) for _ in range(100)]
+
+print(random_numbers)
 
 # Get the length of the list
-n = len(list1)
+list_length = len(random_numbers)
 # Implementing Bubble Sort algorithm to sort the list in ascending order
-for i in range(n):
-    for j in range(0, n - i - 1):  # Iterate through the list, ignoring the last sorted elements
-        if list1[j] > list1[j + 1]: # Compare elements
-            list1[j], list1[j + 1] = list1[j + 1], list1[j] #Swap elements in list
+for pass_num in range(list_length):
+    for index in range(0, list_length - pass_num - 1):  # Iterate through the list, ignoring the last sorted elements
+        if random_numbers[index] > random_numbers[index + 1]: # Compare elements
+            random_numbers[index], random_numbers[index + 1] = random_numbers[index + 1], random_numbers[index] #Swap elements in list
+
+print(random_numbers)
 
 #empty lists to store even and odd numbers
 even_numbers = []
 odd_numbers = []
 
-for num in list1:
+for num in random_numbers:
     if num % 2 == 0: #Check if the number is even
         even_numbers.append(num)
     elif num % 2 != 0: #Check if the number is odd
